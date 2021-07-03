ddf760b438ab48f202a059aaa4b8f3c92aa29e22
dongfg <mail@dongfg.com>
2021-07-03 14:59:38 +0800

    feat: change domain

--- .umirc.prod.ts
+++ .umirc.prod.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   define: {
     'process.env.AUTH_DOMAIN': 'dongfg.auth0.com',
     'process.env.AUTH_CLIENT_ID': 'BqJ86mx1FbobtqjmuPlYg5TOnnd4nQeD',
-    'process.env.AUTH_REDIRECT_URI': 'https://task.dongfg.com/#/callback',
+    'process.env.AUTH_REDIRECT_URI': 'https://task.fun.dongfg.com/#/callback',
     'process.env.AUTH_AUDIENCE': 'https://api.dongfg.com/scheduling',
     'process.env.API_HOST': 'https://api.dongfg.com/scheduling',
   },
